64940ed541f37e90d09aa3afa28f83f197f32736
Joshua Lokken <joshualokken@pm.me>
2022-06-16 09:32:31 -0500

    Child routing

---
+++ src/app/recipes/recipe.service.ts
@@ -0,0 +1,38 @@
+import { EventEmitter, Injectable } from '@angular/core';
+import { Ingredient } from '../shared/ingredient.model';
+import { ShoppingListService } from '../shopping-list/shopping-list.service';
+import { Recipe } from './recipe.model';
+
+@Injectable()
+export class RecipeService {
+  recipeSelected = new EventEmitter<Recipe>();
+
+  private recipes: Recipe[] = [
+    new Recipe(
+      'Test Recipe',
+      'A test',
+      'https://images.unsplash.com/photo-1567620905732-2d1ec7ab7445?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NHx8Zm9vZHxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60',
+      [new Ingredient('Provolone Cheese', 1), new Ingredient('Basil', 2)]
+    ),
+    new Recipe(
+      'Another Test Recipe',
+      'Another test',
+      'https://images.unsplash.com/photo-1555939594-58d7cb561ad1?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8Zm9vZHxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60',
+      [new Ingredient('Cheddar Cheese', 2), new Ingredient('Meat', 3)]
+    ),
+  ];
+
+  constructor(private shoppingListService: ShoppingListService) {}
+
+  getRecipes() {
+    return this.recipes.slice();
+  }
+
+  getRecipe(id: number) {
+    return this.recipes[id];
+  }
+
+  addIngredientsToShoppingList(ingredients: Ingredient[]) {
+    this.shoppingListService.addIngredients(ingredients);
+  }
+}
